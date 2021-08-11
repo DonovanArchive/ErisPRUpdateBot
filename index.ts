ae925b827300e0c82ee6023e27c335cfbcbf3a50
Donovan Daniels <hewwo@yiff.rocks>
2021-08-11 15:28:22 -0500

    no dup pr & close old pr

--- index.ts
+++ index.ts
@@ -15,6 +15,7 @@ const config = YAML.parse(fs.readFileSync(`${__dirname}/config.yml`).toString())
 	remotes: Record<string, string> & { origin: string; };
 };
 
+const ORIGIN_USER = config.remotes.origin.split("/").slice(-2)[0];
 if(!process.env.GITHUB_USER) throw new Error("Missing GITHUB_USER env variable");
 if(!process.env.GITHUB_TOKEN) throw new Error("Missing GITHUB_TOKEN env variable");
 
@@ -38,6 +39,12 @@ process.nextTick(async() => {
 	// add remotes
 	for(const [name, url] of r) await git.addRemote(name, url/* `https://${config.auth.user}:${config.auth.token}@${url.slice(8)}` */);
 
+	const pulls = await octo.request("GET /repos/{owner}/{repo}/pulls", {
+		owner: ORIGIN_USER,
+		repo: "eris"
+	});
+
+	const prRefList = pulls.data.map(d => d.head.ref);
 
 	// loop branches
 	for(const [branch, refs] of b) {
@@ -62,23 +69,53 @@ process.nextTick(async() => {
 				const ls = await git.listRemote([remote, "-h", refBranch]);
 				// slice off last part of ls-remote
 				const hash = ls.toString().slice(0, 40);
+				const prBranch = `${remote}/${refBranch}/${hash}`;
 				// check if latest commit is included in local commits
 				if(!hashes.includes(hash)) {
 					outdated = true;
-					console.log(`Ref "${name}" is outdated, ${remote}/${refBranch} contains hash not included in local: ${hash}`);
-					const prBranch = `${branch}/${remote}/${hash}`;
+					// check if one of the pull request branches matches our current
+					if(prRefList.includes(prBranch)) {
+						console.log(`Ref "${name}" is outdated, but a pull request was already found.`);
+						continue;
+					} else console.log(`Ref "${name}" is outdated, ${remote}/${refBranch} contains hash not included in local: ${hash}`);
+					
+
 					await git.fetch(remote, `${refBranch}:${prBranch}`);
 					await git.checkout(prBranch);
 					await git.push("self", prBranch);
 					const pr = await octo.request("POST /repos/{owner}/{repo}/pulls", {
-							owner: config.remotes.origin.split("/").slice(-2)[0],
-							repo: config.remotes.origin.split("/").slice(-1)[0],
+							owner: ORIGIN_USER,
+							repo: "eris",
 							title: `Remote Update (${branch}): ${remote}/${refBranch}`,
 							head: `${process.env.GITHUB_USER}:${prBranch}`,
 							base: branch,
 							maintainer_can_modify: true
 					});
 					console.log(`Created pull request for "${name}", ${pr.data.html_url}`);
+					// get all remote commits
+					const branchLog = await git.log([`${remote}/${refBranch}`]);
+					const branchHashes = branchLog.all.map(c => c.hash);
+					
+					// check for pull requests with a branch of one of our commit hashes
+					// (new commits since last)
+					const prDupRef = prRefList.find(r => branchHashes.some(h => r.split("/").includes(h)));
+					if(prDupRef) {
+						const oldPr = pulls.data.find(p => p.head.ref === prDupRef)!.number;
+						console.log(`(${remote}/${refBranch}) Closing old pull request #${oldPr}`);
+						await octo.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+							owner: ORIGIN_USER,
+							repo: "eris",
+							issue_number: oldPr,
+							body: `This pull request has been superseded by #${pr.data.number}`
+						});
+						await octo.request("PATCH /repos/{owner}/{repo}/pulls/{pull_number}", {
+							owner: ORIGIN_USER,
+							repo: "eris",
+							pull_number: oldPr,
+							state: "closed"
+						});
+						await git.push(["self", "--delete", prDupRef]);
+					}
 				} else console.log(`Ref "${name}" is up-to-date, hash: ${hash}`);
 			}
 		} catch(err) {
