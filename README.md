85e566561124df86f7aedef74d151fa8979d00c8
Donovan Daniels <hewwo@yiff.rocks>
2021-08-11 06:34:32 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Eris PR Update Bot
+This is an automated script created by [@DonovanDMC](https://github.com/DonovanDMC), ran through [@ErisPRUpdateBot](https://github.com/ErisPRUpdateBot) to automatically check for pr updates for my fork of [Eris](https://github.com/DonovanDMC/eris).
---
+++ index.ts
@@ -0,0 +1,92 @@
+import { execSync } from "child_process";
+import * as fs from "fs-extra";
+import YAML from "yaml";
+import crypto from "crypto";
+import simpleGit from "simple-git/promise";
+import fetch from "node-fetch";
+import { Octokit } from "@octokit/core";
+const jobId = crypto.randomBytes(16).toString("hex");
+const wDir = `${__dirname}/run/${jobId}`;
+fs.removeSync(`${__dirname}/run`); // @FIXME
+fs.mkdirpSync(wDir);
+const config = YAML.parse(fs.readFileSync(`${__dirname}/config.yml`).toString()) as {
+	branches: Record<string, Array<Record<"name" | "branch" | "remote", string>>>;
+	remotes: Record<string, string> & { origin: string; };
+};
+
+if(process.env.GITHUB_USER === undefined) throw new Error("Missing GITHUB_USER env variable");
+if(process.env.GITHUB_TOKEN === undefined) throw new Error("Missing GITHUB_TOKEN env variable");
+
+const octo = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+process.nextTick(async() => {
+	const b = Object.entries(config.branches);
+	const r = Object.entries(config.remotes);
+
+	// setup
+	const git = simpleGit(wDir);
+
+	// clone
+	await git.clone(config.remotes.origin, ".");
+	execSync(`git config --local credential.helper '!f() { sleep 1; echo "username=${process.env.GITHUB_USER}"; echo "password=${process.env.GITHUB_TOKEN}"; }; f'`, {
+		cwd: wDir
+	});
+
+	// override it
+	await git.removeRemote("origin");
+	// add remotes
+	for(const [name, url] of r) await git.addRemote(name, url/* `https://${config.auth.user}:${config.auth.token}@${url.slice(8)}` */);
+
+	// git local commits
+	const log = await git.log();
+	const hashes = log.all.map(c => c.hash);
+
+	// loop branches
+	for(const [branch, refs] of b) {
+	console.log(`Checking the branch "${branch}"`);
+	let outdated = false, currentRef = "unknown";
+		try {
+			// checkout branch
+			await git.fetch("origin", branch);
+			await git.checkout(branch);
+			// await git.branch(["-m", jobId]);
+
+			// loop remotes
+			for(const { name, branch: refBranch, remote } of refs) {
+				currentRef = refBranch;
+				// bring remote branch to local
+				await git.fetch(remote, refBranch);
+				// get remote commits
+				const ls = await git.listRemote([remote, "-h", refBranch]);
+				// slice off last part of ls-remote
+				const hash = ls.toString().slice(0, 40);
+				// check if latest commit is included in local commits
+				if(!hashes.includes(hash)) {
+					outdated = true;
+					console.log(`Ref "${name}" is outdated, ${remote}/${branch} contains hash not included in local: ${hash}`);
+					/* await git.merge(["--commit", "--no-edit", "--allow-unrelated-histories", `${remote}/${branch}`]).catch(err => {
+						conflict = true;
+						console.error("Ref \"%s\" has a merge conflict, this must be fixed on github.", name);
+					});
+					await git.add(".");
+					await git.commit(`Update ref "${name}""`); */
+					await git.fetch(remote, `${refBranch}:update/${remote}/${refBranch}/${jobId}`);
+					await git.push("self", `update/${remote}/${refBranch}/${jobId}`);
+					const pr = await octo.request("POST /repos/{owner}/{repo}/pulls", {
+							owner: config.remotes.origin.split("/").slice(-2)[0],
+							repo: config.remotes.origin.split("/").slice(-1)[0],
+							title: `Remote Update: ${remote}/${refBranch}`,
+							head: `${config.remotes.self.replace(/(https?:\/\/)?(\w:\w)?github.com\//, "").split("/").slice(-2, -1)[0]}:update/${remote}/${refBranch}/${jobId}`,
+							base: branch,
+							maintainer_can_modify: true
+					});
+					console.log(`Created pull request for "${name}", ${pr.data.html_url}`);
+				} else console.log(`Ref "${name}" is up-to-date, hash: ${hash}`);
+			}
+		} catch(err) {
+			console.log(`Error updating "${currentRef}"`, err);
+		}
+
+		console.log(`Done processing for branch "${branch}", ${outdated ? "" : "no "}changes were found`);
+	}
+});
